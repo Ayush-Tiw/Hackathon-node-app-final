9d28307c53d148bddfa174b0b5e447c4fae772a4
Ayush-Tiw <ayushtiwari782@gmail.com>
2022-08-16 22:40:55 +0530

    backend

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient,ObjectId} from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
 import cors from "cors";
+import Stripe from 'stripe';
+const stripe = new Stripe('pk_test_51LUTLESGF1FrCVyXlctlwjhduHJV4yykGSQbY0L3UyHI142XPzC49rCEazVh0Y5wQLFejrh9pWWx3UZ73rb7zWPs00DnTjsKay');
+
+import { v4 as uuidv4 } from 'uuid';
+uuidv4()
 
 
 
@@ -30,7 +35,7 @@ async function createConnection() {
 export const client = await createConnection();
 
 // welcome app
-app.get("/",function(reques,response){
+app.get("/",function(request,response){
   response.send({message:"this is welcome page"})
 })
 
@@ -40,7 +45,7 @@ app.get("/foods/:id", async function (request, response) {
   const food = await client
     .db("hackathon-node-app")
     .collection("foods")
-    .findOne({ id: id });
+    .findOne({ _id: ObjectId(id)});
   food
     ? response.send(food)
     : response.status(404).send({ msg: "food not found" });
@@ -48,13 +53,16 @@ app.get("/foods/:id", async function (request, response) {
 // create food list
 app.post("/foods", async function (request, response) {
   const data = request.body;
-  console.log(data);
+  console.log(request.body)
+  
+  console.log(data)
 
   const result = await client
     .db("hackathon-node-app")
     .collection("foods")
-    .insertMany(data);
+    .insertOne(data);
   response.send(result);
+  console.log(result)
 });
 // get all foods
 app.get("/foods", async function (request, response) {
@@ -65,6 +73,54 @@ app.get("/foods", async function (request, response) {
     .toArray();
   response.send(result);
 });
+// search food by name
+app.get("/foods/:name", async function (request, response) {
+  const {name}=request.params;
+  const result = await client
+    .db("hackathon-node-app")
+    .collection("foods")
+    .find({name:name})
+    .toArray();
+    result ?response.send(result): response.status(404).send({message:"food unavailable"})
+  
+})
+
+// delete food by _id
+
+app.delete("/food/:id",async function(request,response){
+  const {id}=request.params;
+  console.log(request.params,id)
+  const result= await client .db("hackathon-node-app")
+  .collection("foods").deleteOne({_id: ObjectId(id)})
+
+  console.log(result)
+  response.send(result)
+  // result.deletedCount>0 ? response.send({message:"item Deleted SuccesFully"}) :response.status(404).send({message:"item not found"})
+});
+
+// update food by id
+app.put("/foods/:id",async function (request, response) {
+  const {id}=request.params;
+  console.log(request.params,id)
+  const data=request.body;
+  const result = await client
+    .db("hackathon-node-app")
+    .collection("foods")
+    .updateOne({_id: ObjectId(id)},{$set:data})
+  response.send(result);
+})
+
+// update food quantityby id
+app.put("/foods/:id",async function (request, response) {
+  const {id}=request.params;
+  console.log(request.params,id)
+  const data=request.body;
+  const result = await client
+    .db("hackathon-node-app")
+    .collection("foods")
+    .updateOne({_id: objectId(id)},{$set:data})
+  response.send(result);
+})
 
 
 // create restaurant list
@@ -95,13 +151,77 @@ app.get("/restaurants/:id", async function (request, response) {
   const food = await client
     .db("hackathon-node-app")
     .collection("restaurants")
-    .findOne({ id: id });
+    .findOne({_id:ObjectId(id)});
   food
     ? response.send(food)
     : response.status(404).send({ msg: "restaurant not found" });
 });
 
 
+// // create items in cart 
+
+async function getItemByName(name){
+  return await client.db("hackathon-node-app").collection("cart").findOne({name:name})
+}
+
+app.post("/cart", async function (request, response) {
+  
+  const {name,image,price,quantity}=request.body;
+  const itemFromDB=await getItemByName(name);
+  console.log(itemFromDB)
+
+  if(itemFromDB){
+    response.status(400).send({message:"item already exist"})
+    console.log(response.body)
+  }else{
+    const data={
+      name:name,
+      image:image,
+      price:price,
+      quantity:quantity
+    }
+    const result = await client
+    .db("hackathon-node-app")
+    .collection("cart")
+    .insertOne(data);
+  response.send(result);
+  // response.send({message:"item added successfully"})
+  // console.log(request.body)
+  
+  }
+
+  
+});
+
+// get cart
+app.get("/cart", async function (request, response){
+  const result = await client
+  .db("hackathon-node-app")
+  .collection("cart")
+  .find({})
+  .toArray();
+response.send(result);
+
+})
+
+// delete cart item by id
+app.delete("/cart/:id",async function(request,response){
+  const {id}=request.params;
+  console.log(request.params,id)
+  const result= await client .db("hackathon-node-app")
+  .collection("cart").deleteOne({_id: ObjectId(id)})
+
+  console.log(result)
+  response.send(result)
+  // result.deletedCount>0 ? response.send({message:"item Deleted SuccesFully"}) :response.status(404).send({message:"item not found"})
+});
+
+// delete all cart items after checkout
+app.delete("/cart",async function(request,response){
+  const result= await client .db("hackathon-node-app")
+  .collection("cart").deleteMany({})
+  response.send(result)
+})
 
 
 // signup
@@ -112,8 +232,8 @@ async function genHashedPassword(password){
   // console.log(password,salt)
   return hashedPassword;
 }
-async function getUserByName(username){
-  return await client.db("hackathon-node-app").collection("users").findOne({username:username})
+async function getUserByEmail(email){
+  return await client.db("hackathon-node-app").collection("users").findOne({email:email})
 }
 
 app.post("/users/signup",async function(request,response){
@@ -123,16 +243,14 @@ app.post("/users/signup",async function(request,response){
 
  
 
-  const userFromDB=await getUserByName(username);
+  const userFromDB=await getUserByEmail(email);
   console.log(userFromDB)
 
   if(userFromDB){
-    response.status(400).send({message:"username already exist"})
-    console.log(response.body)
+    response.status(400).send({message:"Email already exist"})
+    // console.log(response.body)
   }
-  // else if(password.length<8){
-  //   response.status(400).send({message:"password should be min 8 character"})
-  // }
+  
   else{
     const hashedPassword=await genHashedPassword(password)
     console.log(hashedPassword)
@@ -144,13 +262,14 @@ app.post("/users/signup",async function(request,response){
     }
     const result=client.db("hackathon-node-app").collection("users").insertOne(data)
     response.send(result)
-    response.send({message:"registered successfully"})
+    console.log(result)
+    // response.send({message:"registered successfully"})
   }
 
   
 })
 // get all users
-app.get("/users/signup", async function (request, response) {
+app.get("/users", async function (request, response) {
   const result = await client
     .db("hackathon-node-app")
     .collection("users")
@@ -159,29 +278,173 @@ app.get("/users/signup", async function (request, response) {
   response.send(result);
 });
 
+// get user by id
+app.get("/user/:email",async function(request,response){
+  const{email}=request.params;
+  const result=await client.db("hackathon-node-app").collection("users").findOne({email:email})
+  console.log(result)
+  response.send(result)
+  
+})
+
+// delete user by _id
+app.delete("/user/:id",async function(request,response){
+  const {id}=request.params;
+  console.log(request.params,id)
+  const result= await client .db("hackathon-node-app")
+  .collection("users").deleteOne({_id: ObjectId(id)})
+
+  console.log(result)
+  response.send(result)
+  // result.deletedCount>0 ? response.send({message:"item Deleted SuccesFully"}) :response.status(404).send({message:"item not found"})
+});
+
 // login
 app.post("/users/login",async function(request,response){
-const {username,password}=request.body;
+const {email,password}=request.body;
 
-const userFromDB=await getUserByName(username)
+const userFromDB=await getUserByEmail(email)
 console.log(userFromDB)
 
 if(!userFromDB){
   response.status(401).send({message:"User does not exist"})
+  console.log(response.message)
 }else{
   const storePassword=userFromDB.password;
   const isPasswordMatches=await bcrypt.compare(password,storePassword)
   // console.log(isPasswordMatches)
   if(isPasswordMatches){
-// const token=jwt.sign({id:userFromDB._id},
-//   process.env.SECRET_KEY)
-//   console.log(process.env.SECRET_KEY)
-  response.send({message:"successufll login"})
+const token=jwt.sign({id:userFromDB._id},
+  process.env.SECRET_KEY)
+  console.log(process.env.SECRET_KEY)
+  response.send({message:"succesfull login",token:token,user:userFromDB
+  })
+   console.log(token,userFromDB)
 
   }else{
     response.status(401).send({message:"invalid credentials"})
+    console.log(response.message)
   }
 }
 })
+
+// user profile
+app.get("/user/:email", async function (request, response) {
+  const {email}=request.params;
+  const result = await client
+    .db("hackathon-node-app")
+    .collection("users")
+    .find({email:email})
+    .toArray();
+    result ?response.send(result): response.status(404).send({message:"food unavailable"})
+  console.log(result)
+})
+
+// create orders list
+app.post("/orders",async function(request,response){
+console.log(request)
+  console.log("Request:",request.body)
+  const {token,product}=request.body;
+
+  let error,status;
+
+
+    console.log(token)
+    const customer = await stripe.customers.create({
+      
+      email:token.email,
+      source:token.id
+    })
+    const key=uuid()
+
+    const charge=await stripe.charges.create(
+      {
+        amount:product.price*100,
+        currency:"INR",
+        customer:customer.id,
+        receipt_email:token.email,
+        description:"purchased food",
+        shipping:{
+          name:token.card.name,
+          address:{
+            line1:token.card.address_line1,
+            line2:token.address_line,
+            city:token.card.address_city,
+            country:token.card.address_zip,
+
+          }
+        }
+      },
+      {
+        key,
+      }
+    );
+    console.log("charge:",{charge});
+    status="success"
+ 
+  response.json({error,status})
+ 
+  
+  // const result=await client.db("hackathon-node-app").collection("orders").insertOne(data);
+  // response.send(result)
+  // console.log(request.body)
+})
+
+
+
+// Admin signup
+
+app.post("/admin",async function(request,response){
+  const {username,password,email}=request.body;
+    const hashedPassword=await genHashedPassword(password)
+    console.log(hashedPassword)
+    const data={
+      username:username,
+      email:email, 
+      password:hashedPassword
+    }
+    const result=client.db("hackathon-node-app").collection("admin").insertOne(data)
+    response.send(result)
+    // response.send({message:"registered successfully"})
+  
+console.log(request.body)
+  
+})
+
+// Admin login
+
+async function getAdminByEmail(email){
+  return await client.db("hackathon-node-app").collection("admin").findOne({email:email})
+}
+
+app.post("/admin",async function(request,response){
+  const {email,password}=request.body;
+  
+  const adminFromDB=await getAdminByEmail(email)
+  console.log(adminFromDB)
+  
+  if(!adminFromDB){
+    response.status(401).send({message:"You are not an admin"})
+    console.log(response.message)
+  }else{
+    const storePassword=adminFromDB.password;
+    const isPasswordMatches=await bcrypt.compare(password,storePassword)
+    // console.log(isPasswordMatches)
+    if(isPasswordMatches){
+  const token=jwt.sign({id:adminFromDB._id},
+    process.env.SECRET_KEY)
+    console.log(process.env.SECRET_KEY)
+    response.send({message:"succesfull login",token:token,
+    })
+     console.log(token,adminFromDB)
+  
+    }else{
+      response.status(401).send({message:"invalid credentials"})
+      console.log(response.message)
+    }
+  }
+  })
+  
+
 
 app.listen(PORT, console.log(`app started in port ${PORT}`));
