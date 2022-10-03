de2cc8d7a97267dc097536808a1f1dfa0c195d73
Ayush-Tiw <ayushtiwari782@gmail.com>
2022-10-03 12:50:40 +0530

    c

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ app.get("/cart", async function (request, response) {
 });
 
 // delete cart item by id
-app.delete("/cart/:id", auth, async function (request, response) {
+app.delete("/cart/:id", async function (request, response) {
   const { id } = request.params;
   console.log(request.params, id);
   const result = await client
